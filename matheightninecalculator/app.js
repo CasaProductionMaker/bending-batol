db07905e44d93bf5d0b85486c3645d0b2558ae6f
CasaProductionMaker <74640160+CasaProductionMaker@users.noreply.github.com>
2024-04-19 16:27:59 -0700

    made the math 8 9 calculator /matheightninecalc

--- matheightninecalculator/app.js
+++ matheightninecalculator/app.js
@@ -1,16 +1,50 @@
 let testsNum = [28.75, 24, 26.25];
 let testsDen = [40, 27, 33];
-let thisTestType = ""; //mid-term, final, or normal
+let midTermNum = 47;
+let midTermDen = 50;
+let nextTermDen = 40;
+let thisTestType = "normal"; //mid-term, final, or normal
+let hasDoneMidTerm = true;
+let minimumScore = 75;
 
-let testsTotalNum = 0;
-for (var i = 0; i < testsNum.length; i++) {
-	testsTotalNum += testsNum[i];
+function setTestType(type) {
+	thisTestType = type;
 }
 
-let testsTotalDen = 0;
-for (var i = 0; i < testsDen.length; i++) {
-	testsTotalDen += testsDen[i];
+function calculate_needed() {
+	midTermNum = parseFloat(document.getElementById("midTermScore").value);
+	nextTermDen = parseFloat(document.getElementById("nextTestDen").value);
+	testsNum[0] = parseFloat(document.getElementById("test1score").value);
+	testsNum[1] = parseFloat(document.getElementById("test2score").value);
+	testsNum[2] = parseFloat(document.getElementById("test3score").value);
+	minimumScore = parseFloat(document.getElementById("minScore").value);
+	let testsTotalNum = 0;
+	for (var i = 0; i < testsNum.length; i++) {
+		testsTotalNum += testsNum[i];
+	}
+
+	let testsTotalDen = 0;
+	for (var i = 0; i < testsDen.length; i++) {
+		testsTotalDen += testsDen[i];
+	}
+
+	let testsTotalScore = (testsTotalNum / testsTotalDen) * 100;
+	let midTermScore = (midTermNum / midTermDen) * 100;
+	if(thisTestType == "mid-term")
+	{
+		//0.875 & 0.125
+		alert((minimumScore - (testsTotalScore * 0.875)) / 0.125);
+	} else if(thisTestType == "final")
+	{
+		//0.875 & 0.125
+		alert(((minimumScore - (testsTotalScore * 0.7)) - (midTermScore * 0.1)) / 0.2);
+	} else if(thisTestType = "normal")
+	{
+		if(hasDoneMidTerm)
+		{
+			alert(((((minimumScore - (midTermScore * 0.125)) / 0.875) / 100) * (testsTotalDen + nextTermDen)) - testsTotalScore);
+		} else {
+			alert(((minimumScore / 100) * (testsTotalDen + nextTermDen)) - testsTotalNum);
+		}
+	}
 }
-
-testsTotalScore = (testsTotalNum / testsTotalDen) * 100;
-
